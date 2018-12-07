9e00a919dbc46b8ec78a4560cd3e628e8378f961
PGNQ <41378170+PGNQ@users.noreply.github.com>
2018-12-07 14:46:43 -0500

    implement grocery list page. need to test once pricelist page is done.

--- src/components/grocery-list-page.js
+++ src/components/grocery-list-page.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
 
 class GroceryListPage extends Component {
 
-  render() {
 
+  renderCart() {
+    console.log('rendering cart');
     return (
-      <div className="container">
-        Test
-      </div>
+      <tr key={this.cart.updated_t}>
+        <td>{this.cart.title}</td>
+        <td>{this.cart.merchant}</td>
+        <td>${this.cart.price}</td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Store</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+            {this.renderCart}
+        </tbody>
+      </table>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {cart: state.product.cart};
+}
 
-
-export default GroceryListPage;
+export default connect(mapStateToProps)(GroceryListPage);
